2f143ffa75bb76ce275b1e4ddf6dff5ff2e91d97
gimura2022 <gimura0001@gmail.com>
2024-10-08 04:10:44 +0300

    added 110 chars area lighting

--- lua/base/other.lua
+++ lua/base/other.lua
@@ -20,3 +20,14 @@ vim.cmd [[set ttimeoutlen=10]]
 vim.cmd [[let &t_SI.="\e[5 q"]]
 vim.cmd [[let &t_SR.="\e[3 q"]]
 vim.cmd [[let &t_EI.="\e[1 q"]]
+
+vim.cmd [[
+	if exists('+colorcolumn')
+		highlight ColorColumn ctermbg=235 guibg=#2c2d27
+		highlight CursorLine ctermbg=235 guibg=#2c2d27
+		highlight CursorColumn ctermbg=235 guibg=#2c2d27
+		let &colorcolumn=join(range(110,999),",")
+	else
+		autocmd BufWinEnter * let w:m2=matchadd('ErrorMsg', '\%>110v.\+', -1)
+	end
+]]
